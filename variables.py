5d51c2efb592b74848711764d97780991072bf23
syedomer17 <syedomerali2006@gmail.com>
2025-08-03 16:01:43 +0530

    Updated by Omer on 2025-08-03 at 04:01PM

---
+++ variables.py
@@ -0,0 +1,23 @@
+#python does not have any varirable to like var,let or const 
+
+#we can declare the variables like
+
+x = 5
+y = x
+
+print(x,y)
+
+#Casting
+#If you want to specify the data type of a variable, this can be done with casting.
+
+a = str(3)
+b = int(3)
+c = float(3)
+print(a,b,c)
+
+#Get the Type
+#You can get the data type of a variable with the type() function.
+
+print(type(a))
+print(type(b))
+print(type(c))
