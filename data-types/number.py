521c56654a9c302a793d698c8e41e06e5eb68542
syedomer17 <syedomerali2006@gmail.com>
2025-08-05 15:09:11 +0530

    Updated by Omer on 2025-08-05 at 03:09PM

---
+++ data-types/number.py
@@ -0,0 +1,17 @@
+'''
+Python Numbers
+There are three numeric types in Python:
+
+int
+float
+complex
+Variables of numeric types are created when you assign a value to them:
+'''
+
+x = 1
+y = 1.2
+z = 1j 
+
+print(type(x))
+print(type(y))
+print(type(z))
