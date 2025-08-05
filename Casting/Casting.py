83d96f97259dda988d5aa7247edc5d4e40d55d8a
syedomer17 <syedomerali2006@gmail.com>
2025-08-05 18:16:22 +0530

    Updated by Omer on 2025-08-05 at 06:16PM

---
+++ Casting/Casting.py
@@ -0,0 +1,17 @@
+#int 
+x = int(1) # the value will be one 
+y = int(2.23) # but in this the value will be two because we are taking it as an int type
+z = int('3') # This will convert sting to an integer number 3 int 3
+
+#float
+a = float(1) # This will change the int value to float value 1.0
+b = float(1.111) # This will alwasys be the float numeber because it is already float number
+c = float('3') # This will change the data type form string to float number 3.0
+
+#string
+
+d = str(3) # This will chanage the int value to string
+e = str(1.23) # This will chnage the float value to sting
+f = str('1234') # This will be a string
+
+print(x,y,z,a,b,c,d,e,f)
