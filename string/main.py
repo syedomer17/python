196505a7f1395a89ced0b6c66d62acbf17c26b5f
syedomer17 <syedomerali2006@gmail.com>
2025-08-06 16:03:32 +0530

    Updated by Omer on 2025-08-06 at 04:03PM

--- string/main.py
+++ string/main.py
@@ -6,3 +6,9 @@ print(b)
 
 c = """This is the 2 queation string"""
 print(c)
+
+e = 'Hello, World'
+print(e[2:6]) # so it will take the from Hello maens the 2 is the l and and a is the space
+
+f = ' hello world'
+print(f[-5:-2])
