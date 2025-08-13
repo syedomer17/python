fbf9c5b51f10fd8ca01449e6db860834446bfdb9
syedomer17 <syedomerali2006@gmail.com>
2025-08-13 19:25:13 +0530

    Updated by Omer on 2025-08-13 at 07:25PM

---
+++ operators/main.py
@@ -0,0 +1,27 @@
+print(10+5)
+
+
+"""""
+
+Python divides the operators in the following groups:
+
+Arithmetic operators
+Assignment operators
+Comparison operators
+Logical operators
+Identity operators
+Membership operators
+Bitwise operators
+
+
+"""
+
+'''
+Arithmetic operation +,-,*,%,/,**,//
+'''
+
+addtion = 19 + 10
+print(addtion)
+
+print((6+3) - (10*3))
+print(10 + 2 * 3)
